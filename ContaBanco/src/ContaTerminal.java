5d5b3db38a8f437145d0d470a927ce093a48be97
Miqueias Santiago <Miqueiasengsoft@gmail.com>
2023-09-01 16:44:04 -0300

    Concluido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US); 
 
-        System.out.println("Hello, World!");
-        
+        System.out.print("Nome: ");
+        String nome = sc.next();
+        System.out.print("Numero: ");
+        int id = sc.nextInt();
+        System.out.print("Agencia: ");
+        String agencia = sc.next();
+        System.out.print("Saldo: ");
+        Double saldo = sc.nextDouble();
+
+        System.out.println("Óla " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + id + " e o seu saldo " + saldo + " já esta disponivel para saque.");
+
+        sc.close();
     }
 }
